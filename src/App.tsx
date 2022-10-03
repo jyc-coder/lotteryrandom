725b8a35ebacd1d98cab10424608061f190ad65d
jyc-coder <jyc4648@gmail.com>
2022-10-03 23:39:58 +0900

    #2 - 등수 출력 이벤트 구현
    정답 번호와 랜덤으로 출력된 번호를 비교하여 일치하는 수의 경우에 따라 등수를 출력하는 이벤트를 구현함
    
    * 번호가 5개가 일치해도 목록중에서 보너스 숫자가 존재하면 2등, 아니면 3등으로 간주함
    
    *  `includes`를 사용해서 true가 된 경우에 `matchedNum`의 값을 1씩 더해주며, 반복이 종료된 후의 최종 수치에 따라서 문구가 다르게 출력됨

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,33 @@ const NumberBoxContainer = styled.div`
   margin-top: 30px;
 `;
 
+function getRank(
+  resNum: [number, number, number, number, number, number, number],
+  num: [number, number, number, number, number, number],
+) {
+  const isBonus = num.includes(resNum[6]);
+
+  resNum.splice(6, 1);
+
+  let matchedNum = 0;
+
+  for (const value of num) {
+    if (resNum.includes(value)) matchedNum++;
+  }
+  switch (matchedNum) {
+    case 6:
+      return '축하해요!! 1등입니다!';
+    case 5:
+      return isBonus ? '2등입니다! 그래도 괜찮네요' : '3등입니다';
+    case 4:
+      return '4등입니다';
+    case 3:
+      return '5등입니다';
+    default:
+      return '안타깝지만, 낙첨되셨습니다.';
+  }
+}
+
 function App() {
   const [num, setNum] = useState<
     [number, number, number, number, number, number, number]
@@ -70,6 +97,9 @@ function App() {
                 return <NumberBox key={idx} num={randNum[idx]} />;
               })}
           </NumberBoxContainer>
+          <Title style={{ marginTop: '45px' }}>
+            {getRank([...num], [...randNum])}
+          </Title>
         </>
       )}
     </div>
