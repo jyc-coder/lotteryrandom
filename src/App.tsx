b5bd3332c64a2b5dae087e1f9d1fd35631cbf28b
jyc-coder <jyc4648@gmail.com>
2022-10-03 23:02:27 +0900

    #2- UI 구현
    * style-components 패키지 설치로 js 코드 내부에 css 스타일링을 할수 있게 설정함
    
    * `NumberBox`컴포넌트를 제작함.  1~45 사이의 번호중 임의로 선택하는 `select` 컴포넌트로 제작하여 정답 번호를 만듦
    
    * `App`에서 랜덤 번호 추천의 경우에는 6자리만 타나나게 하고, 임의로 select를 할 이유가 없기 때문에 `setNum`이 존재하지 않으면 `disable`을 `true`로 변경하여 클릭해도 아무런 변화가 없게 설정함
    
    * 랜덤으로 번호를 정하는 것은 `App` 에서  `Button`을 클릭하는 순간 빈 리스트 내부에 무작위 숫자를 집어넣어서 해당 리스트를 바탕으로 `NumberBox`가 렌더링 됨

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+import React, { useState } from 'react';
+import Button from './components/Button';
+import NumberBox from './components/NumberBox';
+import Title from './components/Title';
+
+const NumberBoxContainer = styled.div`
+  display: flex;
+  gap: 16px;
+  margin-top: 30px;
+`;
 
 function App() {
+  const [num, setNum] = useState<
+    [number, number, number, number, number, number, number]
+  >([1, 2, 3, 4, 5, 6, 7]);
+
+  const [randNum, setRandNum] = useState<
+    [number, number, number, number, number, number] | null
+  >(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title>정답 번호</Title>
+      <NumberBoxContainer>
+        {Array(8)
+          .fill(1)
+          .map((_, idx) => {
+            if (idx === 6) return <NumberBox />;
+            if (idx === 7) idx = 6;
+            return (
+              <NumberBox
+                key={idx}
+                num={num[idx]}
+                setNum={(value) => {
+                  if (num.includes(value)) return;
+                  setNum((prev) => {
+                    prev[idx] = value;
+                    return [...prev];
+                  });
+                }}
+              />
+            );
+          })}
+      </NumberBoxContainer>
+
+      <Button
+        onClick={() => {
+          const li: number[] = [];
+          while (li.length < 6) {
+            const v = Math.floor(Math.random() * 45 + 1);
+
+            if (li.includes(v)) continue;
+
+            li.push(v);
+          }
+          setRandNum(li as [number, number, number, number, number, number]);
+        }}
+        style={{ marginTop: '162px' }}
+      >
+        랜덤 번호 추천
+      </Button>
+
+      {randNum && (
+        <>
+          <Title style={{ marginTop: '72px' }}>번호 추첨 결과</Title>
+          <NumberBoxContainer style={{ marginTop: '50px' }}>
+            {Array(6)
+              .fill(1)
+              .map((value, idx) => {
+                return <NumberBox key={idx} num={randNum[idx]} />;
+              })}
+          </NumberBoxContainer>
+        </>
+      )}
     </div>
   );
 }
